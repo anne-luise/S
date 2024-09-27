6bbf22f48178beee076b8cd5d5b90317aa4bd106
anne-luise <130384613+anne-luise@users.noreply.github.com>
2024-09-27 11:21:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let tamanhoSenha = 12;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
-console.log(botoes);
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
     tamanhoSenha = tamanhoSenha - 1;
@@ -19,24 +18,37 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-
-// código omitido
-const campoSenha = document.querySelector("#campo-senha");
-const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 geraSenha();
-function geraSenha(){ 
- let senha = "";
- for (let i = 0; i < tamanhoSenha;i++) {
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
-    console.log(letrasMaiusculas [numeroAleatorio]);
-  }
-  campoSenha.value = senha;
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
 }
-campoSenha.value = letrasMaiusculas;
-
-const checkbox = document.querySelectorAll(‘.checkbox’);
-console.log(checkbox);
-
-
+console.log(alfabeto);
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+}
+    campoSenha.value = senha;
+}
+const checkbox = document.querySelectorAll('.checkbox');
+for (i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
+console.log(checkbox[0].Checked);
