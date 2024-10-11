7e3f6819ddfcf220dccb3e9106f8de580bc65a28
anne-luise <130384613+anne-luise@users.noreply.github.com>
2024-10-11 08:55:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -77,4 +77,13 @@ function classificaSenha(){
     
     }
 
+const valorEntropia = document.querySelector(‘.entropia’);
+    
+    valorEntropia.textContent = “Um computador pode levar até “ +
+Math.floor(2**entropia/(100e6*60*60*24)) + “ dias para descobrir
+essa senha.”;
+    console.log(entropia);
+    }
+        }
+
 
